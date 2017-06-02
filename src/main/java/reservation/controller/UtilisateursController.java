50d799000dd72a1fd25cead53c6bfe7be42c8380
anisbenm <Administrateur@FORMATI-U5T7LNC.formation.at>
2017-06-02 13:56:57 +0200

    1er partie ok

--- src/main/java/reservation/controller/UtilisateursController.java
+++ src/main/java/reservation/controller/UtilisateursController.java
@@ -12,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import reservation.dto.UtilisateurDto;
 import reservation.entity.Utilisateur;
 import reservation.service.UtilisateurCrudService;
 
