1eeaa0cbe90c58b98339112d63dc358782ff1367
anisbenm <Administrateur@FORMATI-U5T7LNC.formation.at>
2017-06-07 11:56:26 +0200

    lister rechercher completé

--- src/main/java/reservation/dto/rechercheDTO.java
+++ src/main/java/reservation/dto/rechercheDTO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package reservation.dto;
+package reservation.dto; 
 
 import java.util.Date;
 
